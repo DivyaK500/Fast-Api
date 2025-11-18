b9c768a1d40ed2c4382c901ae1568418a752b62e
DivyaK500 <153626697+DivyaK500@users.noreply.github.com>
2025-11-18 15:56:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from core.scripts.services.services import router
+
+app = FastAPI()
+
+# Allow Angular frontend to access the API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
